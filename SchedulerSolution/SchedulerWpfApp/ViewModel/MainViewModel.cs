4ef9621a9233cfc6bb48aba1754b164ba8f33599
Nguyen Trong <nguyentrong.se11@gmail.com>
2025-04-29 19:13:56 +0700

    update code MainViewModel

--- SchedulerSolution/SchedulerWpfApp/ViewModel/MainViewModel.cs
+++ SchedulerSolution/SchedulerWpfApp/ViewModel/MainViewModel.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
+using SchedulerWpfApp.Model;
+using SchedulerWpfApp.Services;
 
 namespace SchedulerWpfApp.ViewModel
 {
-    public class MainViewModel
+    public class MainViewModel : ViewBaseModel
     {
+        #region Fields
+        private readonly IPersonService _personService;
+        private ObservableCollection<Person> _people;
+        private Person? _selectedPerson;
+        private Person _newPerson;
+        #endregion
+
+        #region Cotrructor
+        public MainViewModel(IPersonService personService)
+        {
+            _personService = personService;
+            _people = new ObservableCollection<Person>();
+            _newPerson = new Person();
+        }
+        #endregion
     }
 }
