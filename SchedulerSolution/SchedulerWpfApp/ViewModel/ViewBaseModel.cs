e14ae05611b49a898df73379c6e06d5167a408df
Nguyen Trong <nguyentrong.se11@gmail.com>
2025-04-29 19:16:17 +0700

    update code ViewBaseModel

--- SchedulerSolution/SchedulerWpfApp/ViewModel/ViewBaseModel.cs
+++ SchedulerSolution/SchedulerWpfApp/ViewModel/ViewBaseModel.cs
@@ -1,12 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 
 namespace SchedulerWpfApp.ViewModel
 {
-    public class ViewBaseModel
+    /// <summary>
+    /// Base class for all view models in the application.
+    /// Implements INotifyPropertyChanged to support binding in WPF.
+    /// </summary>
+    public class ViewBaseModel : INotifyPropertyChanged
     {
+        #region Fields
+        /// <summary>
+        /// Event that is raised when a property value changes.
+        /// WPF binding system subscribes to this event to be notified of property changes.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Raises the PropertyChanged event for the specified property.
+        /// </summary>
+        /// <param name="propertyName">The name of the property that changed.</param>
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Sets the field value and raises the PropertyChanged event if the value has changed.
+        /// </summary>
+        /// <typeparam name="T">The type of the property.</typeparam>
+        /// <param name="field">Reference to the backing field of the property.</param>
+        /// <param name="value">The new value for the property.</param>
+        /// <param name="propertyName">The name of the property.</param>
+        /// <returns>True if the value was changed, false otherwise.</returns>
+        protected bool SetProperty<T>(ref T field, T value, string propertyName)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value))
+                return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+        #endregion
     }
 }
