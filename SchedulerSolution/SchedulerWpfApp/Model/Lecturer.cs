3620bcc83895454301f58b0e31de118b438d2e90
bangle15092002@gmail.com <bangle15092002@gmail.com>
2025-05-15 13:10:38 +0700

    edit bug

--- SchedulerSolution/SchedulerWpfApp/Model/Lecturer.cs
+++ SchedulerSolution/SchedulerWpfApp/Model/Lecturer.cs
@@ -7,11 +7,10 @@
         public string? Role { get; set; }
         public List<LecturerSubject>? LecturerSubjects { get; set; }
 
-        public Lecturer(string lecturerId, string lecturerName, string? role List<LecturerSubject>? lecturerSubjects = null)
+        public Lecturer(string lecturerId, string lecturerName, List<LecturerSubject>? lecturerSubjects = null)
         {
             LecturerId = lecturerId;
             LecturerName = lecturerName;
-            Role = role;
             LecturerSubjects = lecturerSubjects ?? new List<LecturerSubject>();
         }
     }
