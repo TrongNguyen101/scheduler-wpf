a8b1fe860ac922ea791dfb5a559268919fc492f2
DatTD18 <tranduydat19082003@gmail.com>
2025-05-29 23:27:59 +0700

    update code feature LectureSubject

---
+++ SchedulerSolution/SchedulerWpfApp/Services/ILectureSubjectService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SchedulerWpfApp.Model;
+
+namespace SchedulerWpfApp.Services
+{
+    public interface ILectureSubjectService
+    {
+        Task ImportLectureSubjectFromExcel(List<LecturerSubject> listlecturesubjectFromExcel);
+        Task<List<LecturerSubject>> GetAllAsync();
+        Task<LecturerSubject> GetByIdAsync(int id);
+        Task AddAsync(LecturerSubject lecturerSubject);
+        Task UpdateAsync(LecturerSubject lecturerSubject);
+        Task DeleteAsync(int id);
+
+    }
+}
---
+++ SchedulerSolution/SchedulerWpfApp/Views/LectureSubjectView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace SchedulerWpfApp.Views
+{
+    /// <summary>
+    /// Interaction logic for LectureSubject.xaml
+    /// </summary>
+    public partial class LectureSubjectView : UserControl
+    {
+        public LectureSubjectView()
+        {
+            InitializeComponent();
+        }
+    }
+}
