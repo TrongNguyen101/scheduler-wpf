0b732c490fb813f3c639d6abbd7d18fe5a7b455c
Lê Chí Khiêm <lechikhiem203@gmail.com>
2025-06-13 13:41:07 +0700

    update code

--- SchedulerSolution/SchedulerWpfApp/Algorithm/CreateScheduleTree.cs
+++ SchedulerSolution/SchedulerWpfApp/Algorithm/CreateScheduleTree.cs
@@ -1,4 +1,5 @@
 ﻿using SchedulerWpfApp.Model;
+using SchedulerWpfApp.ServiceRefactor.SubjectServices;
 using SchedulerWpfApp.Services;
 using SchedulerWpfApp.Services.LecturerSubjectServices;
 using SchedulerWpfApp.Services.ScheduleServices;
@@ -13,10 +14,10 @@ namespace SchedulerWpfApp.Algorithm
         private readonly InterfaceScheduleServices _implementScheduleServices;
         private readonly InterfaceLecturerSubjectServices _implementLecturerSubjectServices;
         private readonly InterfaceLecturerServices _implementLecturerServices;
-        private readonly ISubjectServicesOld _subjectServices;
+        private readonly ISubjectServices _subjectServices;
         private readonly IGroupNameService _groupNameService;
 
-        public CreateScheduleTree(GenerateScheduleForAllDate generateScheduleForAllDate, InterfaceScheduleServices implementSchedule, InterfaceLecturerSubjectServices implementLecturerSubjectServices, ISubjectServicesOld subjectServices, InterfaceLecturerServices implementLecturerServices, IGroupNameService groupNameService)
+        public CreateScheduleTree(GenerateScheduleForAllDate generateScheduleForAllDate, InterfaceScheduleServices implementSchedule, InterfaceLecturerSubjectServices implementLecturerSubjectServices, ISubjectServices subjectServices, InterfaceLecturerServices implementLecturerServices, IGroupNameService groupNameService)
         {
             _generateScheduleForAllDate = generateScheduleForAllDate;
             _implementScheduleServices = implementSchedule;
