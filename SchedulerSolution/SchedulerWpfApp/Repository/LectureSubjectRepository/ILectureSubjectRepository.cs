05426ea655bd64b842641ef24557aca38a798c40
DatTD18 <tranduydat19082003@gmail.com>
2025-06-16 18:18:41 +0700

    update code

--- SchedulerSolution/SchedulerWpfApp/Repository/LectureSubjectRepository/ILectureSubjectRepository.cs
+++ SchedulerSolution/SchedulerWpfApp/Repository/LectureSubjectRepository/ILectureSubjectRepository.cs
@@ -4,7 +4,7 @@ namespace SchedulerWpfApp.Repository.LectureSubjectRepository
 {
     public interface ILectureSubjectRepository: IBaseRepository<LecturerSubject>
     {
-        Task<LecturerSubject> GetLectureSubjectByIdAsync(int lecturesubjectId);
+        Task<LecturerSubject> GetLectureSubjectByIdAsync(int lectureSubjectId);
     }
 }
 
