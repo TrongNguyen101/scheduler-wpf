c7f9685bfcb59e92a24fabbeeda1b80c478cf822
DatTD18 <tranduydat19082003@gmail.com>
2025-06-20 18:03:43 +0700

    update code

--- SchedulerSolution/SchedulerWpfApp/ServiceRefactor/GroupNameService/GroupNameService.cs
+++ SchedulerSolution/SchedulerWpfApp/ServiceRefactor/GroupNameService/GroupNameService.cs
@@ -174,7 +174,7 @@ namespace SchedulerWpfApp.ServiceRefactor.GroupNameService
                 if (!string.IsNullOrWhiteSpace(header))
                     headerMap[header] = c;
             }
-            string[] requiredHeaders = { "Groupname", "Khóa", "Kỳ", "BM", "Ngành" };
+            string[] requiredHeaders = { "GroupName", "Khóa", "Kỳ", "BM", "Ngành" };
             foreach (var h in requiredHeaders)
                 if (!headerMap.ContainsKey(h))
                     throw new Exception($"Missing required column: {h}");
@@ -183,7 +183,7 @@ namespace SchedulerWpfApp.ServiceRefactor.GroupNameService
             {
                 var groupname = new GroupClass
                 {
-                    GroupName = sheet[r, headerMap["Groupname"]].Value,
+                    GroupName = sheet[r, headerMap["GroupName"]].Value,
                     CurriculumCode = sheet[r, headerMap["Khóa"]].Value,
                     Department = sheet[r, headerMap["BM"]].Value,
                     Major = sheet[r, headerMap["Ngành"]].Value,
